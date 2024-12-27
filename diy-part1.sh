313fd56faa3145a74a857f938485b8d9dba10895
Xuzheng Li <112696010+tiamo-Li@users.noreply.github.com>
2024-12-27 23:35:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,4 @@
 
 # Add a feed source
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git istore https://github.com/linkease/istore" >> ./feeds.conf.default
