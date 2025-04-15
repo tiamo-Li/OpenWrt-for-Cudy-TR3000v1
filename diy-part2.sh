104669d09ece24236b49f34430134c79f4a33faf
Xuzheng Li <112696010+tiamo-Li@users.noreply.github.com>
2025-04-15 18:44:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_gener
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname and time zones
-sed -i 's/ImmortalWrt/RAX3000Me/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/KG100/g' package/base-files/files/bin/config_generate
 sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
 
 # add Argon theme
