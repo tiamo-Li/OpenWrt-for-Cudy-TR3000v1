d4f7be68a6cbf00d53200a8c7f85e364c1da15b0
Xuzheng Li <112696010+tiamo-Li@users.noreply.github.com>
2025-06-06 18:22:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,9 +7,9 @@ sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_gener
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname and time zones
-sed -i 's/OpenWrt/XR30s/g' package/base-files/files/bin/config_generate
-sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/XR30i/g' package/base-files/files/bin/config_generate
+# sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
 
 # add Argon theme
-git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/luci-app-argon-config
+# git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/luci-app-argon-config
