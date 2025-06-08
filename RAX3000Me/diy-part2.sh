32b5a47545ae5822f0c2eae069307d96c48a5c93
Xuzheng Li <112696010+tiamo-Li@users.noreply.github.com>
2025-06-08 13:35:15 +0800

    Add files via upload

---
+++ RAX3000Me/diy-part2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname and time zones
+sed -i 's/ImmortalWrt/XR30i/g' package/base-files/files/bin/config_generate
+# sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
+
+# add Argon theme
+# git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/luci-app-argon-config
+
+# add shadowsocks-libev
+git clone https://github.com/shadowsocks/openwrt-shadowsocks.git ./package/shadowsocks-libev
+git clone https://github.com/shadowsocks/luci-app-shadowsocks.git ./package/luci-app-shadowsocks
+pushd package/luci-app-shadowsocks/tools/po2lmo
+make && sudo make install
+popd
